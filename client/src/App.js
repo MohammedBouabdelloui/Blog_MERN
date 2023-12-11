ac682fc9cca39f8799d06cc1d8ee3c89e5ed94bd
MOHAMMED BOUABDELLAOUI <118568976+MohammedBouabdelloui@users.noreply.github.com>
2023-12-11 22:58:22 +0100

    clean peoject

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <main>
+        <header>
+          <a href='#' className='logo'>logo</a>
+          <nav>
+            <a href='#'></a>
+          </nav>
+        </header>
+     </main>
+    
+    </>
+  );
+}
+
+export default App;
