468a464fdee0c8fc5bad9c4a555342f0a71c922e
MOHAMMED BOUABDELLAOUI <118568976+MohammedBouabdelloui@users.noreply.github.com>
2023-12-12 00:18:36 +0100

    update pade index

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,66 @@ function App() {
           </nav>
         </header>
         <div className="post">
-          <img src={postImage}></img>
-          <h2>Filmer banque d'images libres de droit</h2>
-          <p>Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+          <div>
+            <img src={postImage}></img>
+          </div>
+          <div className="texts">
+
+            <h2>Filmer banque d'images libres de droit</h2>
+            <p className="info">
+              <a className="author">mohammed simo</a>
+              <time>2023-12-20 16:45</time>
+            </p>
+            <p className="sumary">Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+          </div>
+        </div>
+
+
+        <div className="post">
+          <div>
+            <img src={postImage}></img>
+          </div>
+          <div className="texts">
+
+            <h2>Filmer banque d'images libres de droit</h2>
+            <p className="info">
+              <a className="author">mohammed simo</a>
+              <time>2023-12-20 16:45</time>
+            </p>
+            <p className="sumary">Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+          </div>
+        </div>
+
+
+        <div className="post">
+          <div>
+            <img src={postImage}></img>
+          </div>
+          <div className="texts">
+
+            <h2>Filmer banque d'images libres de droit</h2>
+            <p className="info">
+              <a className="author">mohammed simo</a>
+              <time>2023-12-20 16:45</time>
+            </p>
+            <p className="sumary">Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+          </div>
+        </div>
+
+
+        <div className="post">
+          <div>
+            <img src={postImage}></img>
+          </div>
+          <div className="texts">
+
+            <h2>Filmer banque d'images libres de droit</h2>
+            <p className="info">
+              <a className="author">mohammed simo</a>
+              <time>2023-12-20 16:45</time>
+            </p>
+            <p className="sumary">Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+          </div>
         </div>
      </main>
     
