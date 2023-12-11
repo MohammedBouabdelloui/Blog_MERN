aade8a7e2473fcbcd290a9d2308c203940746ae8
MOHAMMED BOUABDELLAOUI <118568976+MohammedBouabdelloui@users.noreply.github.com>
2023-12-11 23:44:42 +0100

    create nav bar and page index

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import postImage from './images/post1.jpg';
 function App() {
   return (
     <>
@@ -7,9 +7,15 @@ function App() {
         <header>
           <a href='#' className='logo'>logo</a>
           <nav>
-            <a href='#'></a>
+            <a href='#'>Login</a>
+            <a href='#'>Register</a>
           </nav>
         </header>
+        <div className="post">
+          <img src={postImage}></img>
+          <h2>Filmer banque d'images libres de droit</h2>
+          <p>Parcourez 139 673 photos et images disponibles de filmer, ou utilisez les mots-clés camera ou tournage pour trouver plus de photos et images d’exception.</p>
+        </div>
      </main>
     
     </>
